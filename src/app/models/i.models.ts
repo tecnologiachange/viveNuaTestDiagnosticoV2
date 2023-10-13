5687f25440bcdd2f1d0b443671595134a4c3f862
Aric Dayan Gutierrez Sanchez <aric.gutierrez@vivenua.com>
2023-10-13 14:22:55 -0500

    :construction: Ajuste a visualizacion de resultados

--- src/app/models/i.models.ts
+++ src/app/models/i.models.ts
@@ -68,6 +68,7 @@ export interface Hability {
       id: string;
       ref: string;
       label: string;
+      other: string;
     }
   };
 
--- src/app/pages/load-results/load-results.component.ts
+++ src/app/pages/load-results/load-results.component.ts
@@ -19,7 +19,6 @@ export class LoadResultsComponent implements OnInit{
   ngOnInit(): void {
     this.id = this.route.snapshot.paramMap.get('id')!;
     this.getInformationToResults(this.id).then( (res: any) => {  
-      console.log(res);
       this.router.navigateByUrl ('/results' , { state: res } );
     }).catch( _error => {
       console.error(_error);
